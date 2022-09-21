d16fbc88e76ea7e6e85c37de2d78042b3e16eeb8
Tyler <toygrrr@gmail.com>
2022-09-21 10:29:47 -0700

    commit tree 179612b69df2a2e80ee00e92ccd929b976955fef

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,18 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
-
-    render json: post
+    if post.valid?
+      render json: post, status: :ok
+    else
+    render json: {errors: post.errors}, status: :unprocessable_entity
+    end
   end
 
   private
